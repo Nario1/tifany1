5753b0e050c93f25df3a06d73f9d32fc666cb08f
Nario1 <71917543@continental.edu.pe>
2025-04-16 23:05:50 -0500

    Versión con estructura if para determinar el número mayor

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-'''
-Determina el mayor de tres numeros ingresando
-por el teclado
-'''
-
+''' Determina el mayor de tres numeros ingresando por el teclado '''
 numeros = []
 for i in range(1, 4):
     numero = int(input(f"Ingrese el número {i}: "))
     numeros.append(numero)
 
-mayor = sorted(numeros, reverse=True)[0]
+if numeros[0] >= numeros[1] and numeros[0] >= numeros[2]:
+    mayor = numeros[0]
+elif numeros[1] >= numeros[0] and numeros[1] >= numeros[2]:
+    mayor = numeros[1]
+else:
+    mayor = numeros[2]
 
 print(f"El número mayor entre {numeros[0]}, {numeros[1]} y {numeros[2]} es: {mayor}")
